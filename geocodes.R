4397b538191ab5ff7f9caa3e5cae071b5ab08616
mcclainclain <matthew.mcclain08@icloud.com>
2023-10-15 20:14:13 -0500

    updates

--- geocodes.R
+++ geocodes.R
@@ -7,6 +7,11 @@ players = read_csv("players.csv")
 readRenviron("./.Renviron")
 register_google(Sys.getenv("MAPSAPIKEY"))
 
+# Individual cities that need adjustment for geocoding
+players$Hometown[players$Hometown == "Montreal, Wis."] = "Montreal, Wisconsin"
+players$Hometown[players$Hometown == "Howard, Kan."] = "Howard, Kansas"
+
 players_w_geocodes = mutate_geocode(players, Hometown)
+
 
 write_csv(distinct(players_w_geocodes %>% select(Hometown, lon, lat)), "./geocodes.csv")
